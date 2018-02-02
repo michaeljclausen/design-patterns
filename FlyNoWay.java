7d9683e53e1f51e86db5ff1b1f344477bf252aa3
Michael Clausen <michaeljclausen@gmail.com>
2018-02-02 14:54:47 -0800

    Adds FlyBehavior interface, FlyWithWings and FlyNoWay implementations

---
+++ FlyNoWay.java
@@ -0,0 +1,5 @@
+public class FlyNoWay implements FlyBehavior {
+  public void Fly() {
+    System.out.println("I can't fly.");
+  }
+}
---
+++ FlyWithWings.java
@@ -0,0 +1,5 @@
+public class FlyWithWings implements FlyBehavior {
+  public void Fly() {
+    System.out.println("I'm Flying");
+  }
+}
